92ab9d24b13dd09df7e9fd568d30d1ee29877ce0
Ayush Gusain <ayush5252ag@gmail.com>
2026-04-08 11:22:55 +0530

    fix: music, osd-section added to landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { HeroSection } from "./_components/hero-section";
+import { OsdSection } from "./_components/osd-section";
+
 export default function Home() {
   return (
     <div className="bg-black">
-      Hello
+      <HeroSection />
+      <OsdSection />
     </div>
   )
 }
